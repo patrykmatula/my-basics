1b83a009d0e271c7ab09d50c6992404f611a3697
Patryk Matuła <60926409+patrykmatula@users.noreply.github.com>
2020-02-19 17:45:14 +0100

    ask how many students are in class

---
+++ studentsStatistics.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
+
+public class studentsStatistics {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int howManystudents;
+
+        System.out.print("How many students are in class: ");
+        howManystudents = scanner.nextInt();
+        int[] studentsScore = {howManystudents};
+
+        
+    }
+}
