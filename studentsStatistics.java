7f576d6cdecdec2d5f8c4142193c2a701f8d2635
Patryk Matuła <60926409+patrykmatula@users.noreply.github.com>
2020-02-19 18:54:54 +0100

    ask about student's score in %

--- studentsStatistics.java
+++ studentsStatistics.java
@@ -11,10 +11,14 @@ public class studentsStatistics {
         System.out.print("How many students are in class: ");
         int howManystudents = scanner.nextInt();
         int[] studentsScore = new int[howManystudents];
+        double[] studentsPercentage = new double[howManystudents];
+
 
         for (int studentCounter = 0; studentCounter < howManystudents; studentCounter++) {
-            System.out.print("Student number " + (studentCounter + 1) + " score: ");
+            System.out.print("Student number " + (studentCounter + 1) + " score in 1-6: ");
             studentsScore[studentCounter] = scanner.nextInt();
+            System.out.print("Student number " + (studentCounter + 1) + " score in %: ");
+            studentsPercentage[studentCounter] = scanner.nextDouble();
         }
 
         
